a3e6bc3fefb3860de5ae15079d6b61797307f7e0
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-07-07 14:59:05 +0800

    feat: add choice menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,45 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isExit = false;
+
+        do {
+            System.out.println("--- Grocery Inventory Menu ---");
+            System.out.println("1. View Inventory");
+            System.out.println("2. Add Product");
+            System.out.println("3. Check Product");
+            System.out.println("4. Update Stock");
+            System.out.println("5. Remove Product");
+            System.out.println("6. Exit");
+
+            System.out.print("Choose an option: ");
+            int userChoice = scanner.nextInt();
+            scanner.nextLine(); // Consume new line
+
+            switch (userChoice) {
+                case 1:
+                    // View Inventory
+                    break;
+                case 2:
+                    // Add Product
+                    break;
+                case 3:
+                    // Check Product
+                    break;
+                case 4:
+                    // Update Stock
+                    break;
+                case 5:
+                    // Remove Product
+                    break;
+                default:
+                    isExit = true;
+                    System.out.println("Exiting system...");
+                    break;
+            }
+        } while (!isExit);
+
 
     }
 }
